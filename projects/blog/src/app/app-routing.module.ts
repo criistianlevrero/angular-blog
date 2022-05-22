dd6fe4ddb701272c1b8af6da75214bc785007922
Cristian Levrero <cristianlevrero@gmail.com>
2022-05-22 20:21:26 -0300

    feat: adding backend

--- projects/blog/src/app/app-routing.module.ts
+++ projects/blog/src/app/app-routing.module.ts
@@ -3,12 +3,21 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './pages/about/about.component';
 import { HomeComponent } from './pages/home/home.component';
 import { MainComponent } from './pages/main/main.component';
+import { PostComponent } from './pages/post/post.component';
 
 const routes: Routes = [
   { path: '', component: MainComponent,
   children: [
     { path: '', component: HomeComponent, },
-    { path: 'about', component: AboutComponent }
+    { path: 'about', component: AboutComponent },
+    { path: 'post', component: PostComponent,
+      children:[
+        {
+          path:':postId', //:type is dynamic here
+          component:PostComponent
+        }
+      ]
+    }
   ]},
 ];
 
